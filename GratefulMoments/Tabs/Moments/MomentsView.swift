b77372aff296f795f56d557241792569d69f1945
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 21:07:35 +0530

    displayed data in a list

---
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentsView.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import SwiftUI
+
+struct MomentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentsView()
+}
