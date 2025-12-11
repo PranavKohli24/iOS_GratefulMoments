5d18ec0a283eebe216ea5e479ad030f2949554e4
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 20:30:08 +0530

    created a data container

---
+++ GratefulMoments/Logic/DataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DataContainer.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import Foundation
---
+++ GratefulMoments/Models/Moment.swift
@@ -0,0 +1,8 @@
+//
+//  Moment.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import Foundation
