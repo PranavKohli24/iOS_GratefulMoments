bd8ec2b99bf61c8da53d13189641cfe4a1bba3d8
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 18:25:16 +0530

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
