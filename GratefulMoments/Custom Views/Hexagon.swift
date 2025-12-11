2b7d66f3ca8b38e8d88465865a425540345dc800
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 21:14:24 +0530

    created a custom view component

---
+++ GratefulMoments/Custom Views/Hexagon.swift
@@ -0,0 +1,18 @@
+//
+//  Hexagon.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import SwiftUI
+
+struct Hexagon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hexagon()
+}
