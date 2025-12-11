2b348169bce58f68fcce42ad6842549613c6a412
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 19:51:09 +0530

    Built data entry view

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct MomentEntryView: View {
+    
+    @State private var title = ""
+    @State private var note = ""
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView{
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            
+            .navigationTitle("Grateful For")
+        }
     }
+    
+    var contentStack: some View {
+            VStack(alignment: .leading) {
+                
+                TextField(text: $title) {
+                               Text("Title (Required)")
+                           }
+                           .font(.title.bold())
+                           .padding(.top, 48)
+                
+                Divider()
+                
+                TextField("Log your small wins", text: $note, axis: .vertical)
+                               .multilineTextAlignment(.leading)
+                               .lineLimit(5...Int.max)
+            }
+            .padding()
+        }
 }
 
 #Preview {
