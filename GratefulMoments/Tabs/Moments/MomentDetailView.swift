a5ae5f757fd3d19c6a3aa126fb3c97d0d6a40653
Pranav Kohli <kohlipranav24@gmail.com>
2025-12-11 21:11:16 +0530

    Built a detail view

---
+++ GratefulMoments/Tabs/Moments/MomentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentDetailView.swift
+//  GratefulMoments
+//
+//  Created by Vaibhav Monga on 11/12/25.
+//
+
+import SwiftUI
+
+struct MomentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentDetailView()
+}
